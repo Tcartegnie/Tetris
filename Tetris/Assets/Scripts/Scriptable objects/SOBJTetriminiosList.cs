6f6d60fbf6289002795671530f77db07847b9cd1
Tcartegnie <Sorakssis@gmail.com>
2021-07-02 03:02:31 +0200

    Add new scriptable object to manage Tetriminos list

---
+++ Tetris/Assets/Scripts/Scriptable objects/SOBJTetriminiosList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TetriminosList", order = 1)]
+public class SOBJTetriminiosList : ScriptableObject
+{
+	public List<SOBJTetriminos> Tetriminos = new List<SOBJTetriminos>();
+
+	public SOBJTetriminos GetRandomTetriminos()
+	{
+		return	Tetriminos[Random.Range(0,Tetriminos.Count)];
+	}
+}
